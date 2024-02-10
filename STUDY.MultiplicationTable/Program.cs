4154b14d90a375acb0e12a9ae901297bf350a7d4
Eduardo Henrique <newdamage1@live.com>
2024-02-10 13:27:47 -0300

    Adicionar arquivos de projeto.

---
+++ STUDY.MultiplicationTable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Table of 2.");
+
+            int number = 2;
+            
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int result;
+
+                result = number * i;
+
+                Console.WriteLine("{0} x {1} = {2}", number, i, result);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
